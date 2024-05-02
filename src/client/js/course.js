fd25b1bfcecd6c84f869706e63ca7fd40d705f2b
parkjunhyun <jhyun970405@gmail.com>
2024-05-02 16:39:04 +0900

    aa

--- src/client/js/course.js
+++ src/client/js/course.js
@@ -6,10 +6,35 @@ let userLongitude;
 let isMapDrawn = false; // boolean
 let courseData = [];
 let markers = [];
+let clickCourse = 0; //0 내자신, 나머지 id
 
 const panTo = (latitude, longitude) => {
-  const moveLatLon = new kakao.maps.LatLng(latitude, longitude);
+  const position = new kakao.maps.LatLng(latitude, longitude);
   map.panTo(position);
+};
+
+const clickCourseList = (e, courseNo) => {
+  console.log(courseNo)
+  if(clickCourse !== courseNo){
+    const courseWrap = document.querySelectorAll(".course");
+    for(let i =0; i < courseWrap.length; i++){
+      courseWrap[i].classList.remove("on");
+    }
+    // 클릭한것 색칠
+    e.currentTarget.classList.add("on");
+    let courselatitude;
+    let courselongitude
+    if(courseNo === 0){
+      courselatitude = userLatitude;
+      courselongitude = userLongitude;
+    }else{
+      const mastchCourse = courseData.find(c => c.course_no === courseNo);
+      courselatitude = mastchCourse.course_latitude;
+      courselongitude = mastchCourse.course_longitude;
+    }   
+    panTo(courselatitude, courselongitude);
+    clickCourse = courseNo;
+  }
 };
 
 // 마커 그리는 함수
@@ -78,6 +103,7 @@ const configLocation = () => {
         isMapDrawn = true;
       }
       addMarker(new kakao.maps.LatLng(userLatitude, userLongitude))
+      panTo(userLatitude, userLongitude);
     });
   }
 };
@@ -86,11 +112,11 @@ const makeCourseNaviHTML = (data) => {
   const courseWrap = document.getElementById("courseWrap");
   let html = "";
   for (let i = 0; i < data.length; i++) {
-    html += `<li class="course">`;
+    html += `<li class="course" onclick="clickCourseList(event,${data[i].course_no})">`;
     html += `<p>${data[i].course_name}</p>`;
     html += `</li>`;
   }
-  html += `<li id="myPosition" class="course on">나의 위치</li>`;
+  html += `<li id="myPosition" class="course on"  onclick="clickCourseList(event, 0)">나의 위치</li>`;
 
   courseWrap.innerHTML = html;
 };
